d7f7916372da5ed79a339798d6a804b3fe424625
Gonçalo Santos <a27985@alunos.ipca.pt>
2024-12-02 10:12:06 +0000

    Add project files.

---
+++ Medico.cs
@@ -0,0 +1,95 @@
+﻿/*
+*	<copyright file="TrabalhoPOO_Simples.cs" company="IPCA">
+*		Copyright (c) 2024 All Rights Reserved
+*	</copyright>
+* 	<author>gonca</author>
+*   <date>12/1/2024 6:21:02 PM</date>
+*	<description></description>
+**/
+using System;
+
+namespace TrabalhoPOO_Simples
+{
+    public enum ESPECIALIDADE
+    {
+        Ginecologia = 0,
+        Neurologia = 1,
+        Cardiologia = 2,
+    }
+    /// <summary>
+    /// Purpose:
+    /// Created by: gonca
+    /// Created on: 12/1/2024 6:21:02 PM
+    /// </summary>
+    /// <remarks></remarks>
+    /// <example></example>
+    public class Medico : Pessoa
+    {
+        #region Attributes
+        int crm;
+        ESPECIALIDADE especialidade;
+        
+
+        #endregion
+
+        #region Methods
+
+        #region Constructors
+
+        /// <summary>
+        /// The default Constructor.
+        /// </summary>
+        public Medico()
+        {
+        }
+        
+        public Medico(string nome, DateTime dataN, int nif, Morada morada, int crm, ESPECIALIDADE especialidade)
+        {
+            base.Nome = nome;
+            base.DataN = dataN;
+            base.NIF = nif;
+            base.Morada = morada;
+            this.crm = crm;
+            this.especialidade = especialidade;
+        }
+
+        #endregion
+
+        #region Properties
+        public int CRM
+        {
+            get { return crm; }
+            set { crm = value; }
+        }
+
+        public ESPECIALIDADE Especialidade
+        {
+            get { return especialidade; }
+            set { especialidade = value; }
+        }
+        #endregion
+
+
+
+        #region Overrides
+        #endregion
+
+        #region OtherMethods
+        public Medico CriaMedico(string nome, DateTime dataN, int nif, Morada morada, int crm, ESPECIALIDADE especialidade)
+        {
+            return new Medico(nome, dataN, nif, morada, crm, especialidade);
+        }
+        #endregion
+
+        #region Destructor
+        /// <summary>
+        /// The destructor.
+        /// </summary>
+        ~Medico()
+        {
+        }
+        #endregion
+
+        #endregion
+    }
+}
---
+++ Pessoa.cs
@@ -0,0 +1,93 @@
+﻿/*
+*	<copyright file="TrabalhoPOO_Simples.cs" company="IPCA">
+*		Copyright (c) 2024 All Rights Reserved
+*	</copyright>
+* 	<author>gonca</author>
+*   <date>12/1/2024 6:21:23 PM</date>
+*	<description></description>
+**/
+using System;
+
+namespace TrabalhoPOO_Simples
+{
+    /// <summary>
+    /// Purpose:
+    /// Created by: gonca
+    /// Created on: 12/1/2024 6:21:23 PM
+    /// </summary>
+    /// <remarks></remarks>
+    /// <example></example>
+    public class Pessoa
+    {
+        #region Attributes
+        string nome;
+        DateTime dataN;
+        int nif;
+        Morada morada;
+        #endregion
+
+        #region Methods
+
+        #region Constructors
+
+        /// <summary>
+        /// The default Constructor.
+        /// </summary>
+        public Pessoa()
+        {
+        }
+        public Pessoa(string n, DateTime data, int ni, Morada m)
+        {
+            nome = n;
+            dataN = data;
+            morada = m;
+            nif = ni;
+        }
+
+        #endregion
+
+        #region Properties
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public DateTime DataN
+        {
+            get { return dataN; }
+            set { dataN = value; }
+        }
+
+        public Morada Morada
+        {
+            get { return morada; }
+            set { morada = value; }
+        }
+
+        public int NIF
+        {
+            get { return nif; }
+            set { nif = value; }
+        }
+        #endregion
+
+
+
+        #region Overrides
+        #endregion
+
+        #region OtherMethods
+        #endregion
+
+        #region Destructor
+        /// <summary>
+        /// The destructor.
+        /// </summary>
+        ~Pessoa()
+        {
+        }
+        #endregion
+
+        #endregion
+    }
+}
